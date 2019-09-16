3714755f02577872248bfbf6546620eebf07348d
Morten Jansrud <mjansrud@users.noreply.github.com>
2019-09-16 11:46:00 +0200

    Import riktige header funksjoner

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const port = 8080;
 const BASE_URL = "/person/pdl-fullmakt-api";
 const VAULT_PATH = "/var/run/secrets/nais.io/vault/environment.env";
 const dotenv = require("dotenv").config({ path: VAULT_PATH });
-const { setEnheterProxyHeaders, setMottakProxyHeaders } = require("./headers");
+const { setProxyHeaders } = require("./headers");
 
 app.use(cookies());
 app.get(`${BASE_URL}/internal/isAlive`, (req, res) => res.sendStatus(200));
@@ -17,7 +17,7 @@ app.use(
   proxy(`${BASE_URL}/mottak`, {
     target: process.env.TILBAKEMELDINGSMOTTAK_URL,
     pathRewrite: { "^/person/pdl-fullmakt-api/mottak": "" },
-    onProxyReq: setMottakProxyHeaders,
+    onProxyReq: setProxyHeaders,
     changeOrigin: true
   })
 );
