1a84b2531d31550c68dbefe1215f65926a14c4ec
Rituvesh Kumar <rituvesh@gmail.com>
2019-10-17 14:13:00 +0200

    Lagt fodselsnr og brukt sts token i base_url

--- src/server.js
+++ src/server.js
@@ -8,14 +8,19 @@ const BASE_URL = "/person/pdl-fullmakt-api";
 const VAULT_PATH = "/var/run/secrets/nais.io/vault/environment.env";
 const dotenv = require("dotenv").config({ path: VAULT_PATH });
 const { setProxyHeaders } = require("./headers");
+const { getStsToken } = require("./ststoken");
 
 app.use(cookies());
 app.get(`${BASE_URL}/internal/isAlive`, (req, res) => res.sendStatus(200));
 app.get(`${BASE_URL}/internal/isReady`, (req, res) => res.sendStatus(200));
+app.get(`${BASE_URL}/fodselsnr`, (req, res) =>
+    res.send({ fodselsnr: decodeJWT(req.cookies["selvbetjening-idtoken"]).sub })
+);
 
 app.use(
+  getStsToken(`${BASE_URL}`),
   proxy(`${BASE_URL}`, {
-    target: process.env.PDL_FULLMAKT_API_URL ,
+    target: process.env.PDL_FULLMAKT_API_URL,
     pathRewrite: { "^/person/pdl-fullmakt-api": "" },
     onProxyReq: setProxyHeaders,
     changeOrigin: true
