4bba2f4f6f97cfec11bf4b33878ebf0aeb7e9b7b
Rituvesh Kumar <rituvesh@gmail.com>
2019-12-11 08:26:28 +0100

    Fjernet sts token

--- src/server.js
+++ src/server.js
@@ -8,7 +8,6 @@ const BASE_URL = "/person/pdl-fullmakt-api";
 const VAULT_PATH = "/var/run/secrets/nais.io/vault/environment.env";
 const dotenv = require("dotenv").config({ path: VAULT_PATH });
 const { setProxyHeaders } = require("./headers");
-const { getStsToken } = require("./ststoken");
 
 app.use(cookies());
 app.get(`${BASE_URL}/internal/isAlive`, (req, res) => res.sendStatus(200));
@@ -18,7 +17,6 @@ app.get(`${BASE_URL}/fodselsnr`, (req, res) =>
 );
 
 app.use(
-  getStsToken(`${BASE_URL}`),
   proxy(`${BASE_URL}`, {
     target: process.env.PDL_FULLMAKT_API_URL,
     pathRewrite: { "^/person/pdl-fullmakt-api": "" },
