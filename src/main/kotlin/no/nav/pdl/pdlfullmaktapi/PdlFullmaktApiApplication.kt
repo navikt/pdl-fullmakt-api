d0fdc3f9a4b03742a70836c18b137615ebc1ab5c
Rituvesh Kumar <rituvesh.kumar@nav.no>
2022-02-21 12:01:06 +0100

    lagt kode fra https://start.spring.io/

---
+++ src/main/kotlin/no/nav/pdl/pdlfullmaktapi/PdlFullmaktApiApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.pdl.pdlfullmaktapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PdlFullmaktApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<PdlFullmaktApiApplication>(*args)
+}
---
+++ src/test/kotlin/no/nav/pdl/pdlfullmaktapi/PdlFullmaktApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.nav.pdl.pdlfullmaktapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PdlFullmaktApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
